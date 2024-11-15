80694e6a3e371161a49b62b918ad6190f0d4b6c0
jakovd <jakov.duzevic@gmail.com>
2024-11-15 13:57:11 +0100

    added alternative path for editor's Images folder from Packages

--- Editor/TexturesUtility.cs
+++ Editor/TexturesUtility.cs
@@ -7,12 +7,18 @@ namespace GaussianSplatting.Editor
     public static class TexturesUtility
     {
         private const string TexturesPath = "Assets/404-gen-unity-plugin/Editor/Images/";
+        private const string AlternativeTexturesPath = "Packages/xyz.404.404-gen-unity-plugin/Editor/Images/";
 
         public static void LoadTexture(ref Texture2D texture, string textureName)
         {
             if (texture == null)
             {
                 var texturePath = Path.Combine(TexturesPath, textureName);
+                texture = AssetDatabase.LoadAssetAtPath<Texture2D>(texturePath);
+            }
+            if (texture == null)
+            {
+                var texturePath = Path.Combine(AlternativeTexturesPath, textureName);
                 texture = AssetDatabase.LoadAssetAtPath<Texture2D>(texturePath);
             }
         }
