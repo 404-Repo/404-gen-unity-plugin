634bec83ad2ba3e2b0132e0e349d285c871e8fa5
jakovd <jakov.duzevic@gmail.com>
2025-02-18 22:29:23 +0100

    adds importing support packs for URP & HDRP from .unitypackage files

--- Editor/404GenTool.cs
+++ Editor/404GenTool.cs
@@ -910,30 +910,75 @@ namespace GaussianSplatting.Editor
 #endif
             m_showRenderingSetup = true;
         }
-        
+
+        private GameObject _gaussianSplatURPPassPrefab;
+        private GameObject _gaussianSplatEffectPrefab;
         private void DrawRenderingSetup()
         {
             if (!m_showRenderingSetup) return;
 #if GS_ENABLE_URP
-            EditorGUILayout.HelpBox(
-                "To add Gaussian splats to the URP rendering process, a custom Render pass must enqueued when camera starts rendering the scene. " +
-                "\n\nClick here to add a gameobject that enqueues custom rendering pass by instantiating a prefab named 'GaussianSplatURPPass' from the Resources folder.",
-                MessageType.Warning);
-
-            if (GUILayout.Button("Add custom rendering pass"))
+            EditorGUILayout.HelpBox("To add Gaussian splats to the URP rendering process, a custom Render pass must be enqueued when camera starts rendering the scene. ",
+                MessageType.Info);
+            
+            
+            if (_gaussianSplatURPPassPrefab == null)
             {
-                AddCustomRenderingPassToCamera();
+                //attempt loading
+                _gaussianSplatURPPassPrefab = Resources.Load<GameObject>("GaussianSplatURPPass");
+                if (_gaussianSplatURPPassPrefab == null)
+                {
+                    EditorGUILayout.HelpBox(
+                        "Load 'HDRP and URP Support Packs/URP Support pack.unitypackage' to enable custom render pass to be added.",
+                        MessageType.Warning);
+                    if (GUILayout.Button("Load URP Support pack"))
+                    {
+                        SupportPacksUtility.ImportPackage("URP Support pack.unitypackage");
+                    }
+                }
+            }
+            else
+            {
+                EditorGUILayout.HelpBox(
+                    "Click here to add a gameobject that enqueues custom rendering pass by instantiating a prefab named 'GaussianSplatURPPass' from the Resources folder.",
+                    MessageType.Warning);
+                // Load the prefab from the Resources folder
+                if (GUILayout.Button("Add custom rendering pass"))
+                {
+                    AddCustomRenderingPassToCamera();
+                }
             }
             
 #elif GS_ENABLE_HDRP
             EditorGUILayout.HelpBox(
-                    "To add Gaussian splats to the HDRP rendering process, a CustomPassVolume must be present in the scene. " +
-                    "Click here to add a preconfigured CustomPassVolume by instantiating a prefab named 'GaussianSplatEffect' from the Resources folder.",
-                    MessageType.Warning);
+                    "To add Gaussian splats to the HDRP rendering process, a CustomPassVolume must be present in the scene. ",
+                    MessageType.Info);
 
-            if (GUILayout.Button("Add HDRP custom pass"))
+            if (_gaussianSplatEffectPrefab == null)
             {
-                AddGaussianSplatEffect();
+                // Load the prefab from the Resources folder
+                _gaussianSplatEffectPrefab = Resources.Load<GameObject>("GaussianSplatEffect");
+                if (_gaussianSplatEffectPrefab == null)
+                {
+                    EditorGUILayout.HelpBox(
+                        "Load 'HDRP and URP Support Packs/HDRP Support pack.unitypackage' to enable CustomPassVolume to be added.",
+                        MessageType.Warning);
+                    
+                    if (GUILayout.Button("Load HDRP Support pack"))
+                    {
+                        SupportPacksUtility.ImportPackage("HDRP Support pack.unitypackage");
+                    }
+                }
+                else
+                {
+                    EditorGUILayout.HelpBox(
+                        "Click here to add a preconfigured CustomPassVolume by instantiating a prefab named 'GaussianSplatEffect' from the Resources folder.",
+                        MessageType.Warning);
+
+                    if (GUILayout.Button("Add HDRP custom pass"))
+                    {
+                        AddGaussianSplatEffect();
+                    }
+                }
             }
 #endif
             GUILayout.Space(20);
@@ -941,17 +986,14 @@ namespace GaussianSplatting.Editor
         
         private void AddGaussianSplatEffect()
         {
-            // Load the prefab from the Resources folder
-            GameObject prefab = Resources.Load<GameObject>("GaussianSplatEffect");
-
-            if (prefab == null)
+            if (_gaussianSplatEffectPrefab == null)
             {
                 Debug.LogError("GaussianSplatEffect prefab not found in Resources folder. Please ensure the prefab is correctly placed in a 'Resources' folder.");
                 return;
             }
 
             // Instantiate the prefab into the scene
-            GameObject instance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+            GameObject instance = PrefabUtility.InstantiatePrefab(_gaussianSplatEffectPrefab) as GameObject;
 
             if (instance != null)
             {
@@ -969,16 +1011,14 @@ namespace GaussianSplatting.Editor
         private void AddCustomRenderingPassToCamera()
         {
             // Load the prefab from the Resources folder
-            GameObject prefab = Resources.Load<GameObject>("GaussianSplatURPPass");
-
-            if (prefab == null)
+            if (_gaussianSplatURPPassPrefab == null)
             {
                 Debug.LogError("GaussianSplatURPPass prefab not found in Resources folder. Please ensure the prefab is correctly placed in a 'Resources' folder.");
                 return;
             }
 
             // Instantiate the prefab into the scene
-            GameObject instance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+            GameObject instance = PrefabUtility.InstantiatePrefab(_gaussianSplatURPPassPrefab) as GameObject;
 
             if (instance != null)
             {
