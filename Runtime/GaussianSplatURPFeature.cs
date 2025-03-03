1af0d04736249792dd87d05cf6b3dc2699dee858
jakovd <jakov.duzevic@gmail.com>
2025-03-03 14:59:37 +0100

    add Render Graph version of Rendering pass for URP

--- Runtime/GaussianSplatURPFeature.cs
+++ Runtime/GaussianSplatURPFeature.cs
@@ -6,6 +6,10 @@ using UnityEngine.Experimental.Rendering;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
+#if UNITY_6000_0_OR_NEWER
+using UnityEngine.Rendering.RenderGraphModule;
+#endif
+
 namespace GaussianSplatting.Runtime
 {
     // Note: I have no idea what is the purpose of ScriptableRendererFeature vs ScriptableRenderPass, which one of those
@@ -13,6 +17,95 @@ namespace GaussianSplatting.Runtime
     // without understanding any of it.
     //
     // ReSharper disable once InconsistentNaming
+    #if UNITY_6000_0_OR_NEWER
+    class GaussianSplatURPFeature : ScriptableRendererFeature
+    {
+        public class GSRenderPass : ScriptableRenderPass
+        {
+            const string GaussianSplatRTName = "_GaussianSplatRT";
+            const string ProfilerTag = "GaussianSplatRenderGraph";
+            static readonly ProfilingSampler s_profilingSampler = new(ProfilerTag);
+            static readonly int s_gaussianSplatRT = Shader.PropertyToID(GaussianSplatRTName);
+
+            class PassData
+            {
+                internal UniversalCameraData CameraData;
+                internal TextureHandle SourceTexture;
+                internal TextureHandle SourceDepth;
+                internal TextureHandle GaussianSplatRT;
+            }
+
+            public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+            {
+                using var builder = renderGraph.AddUnsafePass(ProfilerTag, out PassData passData);
+
+                var cameraData = frameData.Get<UniversalCameraData>();
+                var resourceData = frameData.Get<UniversalResourceData>();
+
+                RenderTextureDescriptor rtDesc = cameraData.cameraTargetDescriptor;
+                rtDesc.depthBufferBits = 0;
+                rtDesc.msaaSamples = 1;
+                rtDesc.graphicsFormat = GraphicsFormat.R16G16B16A16_SFloat;
+                var textureHandle = UniversalRenderer.CreateRenderGraphTexture(renderGraph, rtDesc, GaussianSplatRTName, true);
+
+                passData.CameraData = cameraData;
+                passData.SourceTexture = resourceData.activeColorTexture;
+                passData.SourceDepth = resourceData.activeDepthTexture;
+                passData.GaussianSplatRT = textureHandle;
+
+                builder.UseTexture(resourceData.activeColorTexture, AccessFlags.ReadWrite);
+                builder.UseTexture(resourceData.activeDepthTexture);
+                builder.UseTexture(textureHandle, AccessFlags.Write);
+                builder.AllowPassCulling(false);
+                builder.SetRenderFunc(static (PassData data, UnsafeGraphContext context) =>
+                {
+                    var commandBuffer = CommandBufferHelpers.GetNativeCommandBuffer(context.cmd);
+                    using var _ = new ProfilingScope(commandBuffer, s_profilingSampler);
+                    commandBuffer.SetGlobalTexture(s_gaussianSplatRT, data.GaussianSplatRT);
+                    CoreUtils.SetRenderTarget(commandBuffer, data.GaussianSplatRT, data.SourceDepth, ClearFlag.Color, Color.clear);
+                    Material matComposite = GaussianSplatRenderSystem.instance.SortAndRenderSplats(data.CameraData.camera, commandBuffer);
+                    commandBuffer.BeginSample(GaussianSplatRenderSystem.s_ProfCompose);
+                    Blitter.BlitCameraTexture(commandBuffer, data.GaussianSplatRT, data.SourceTexture, matComposite, 0);
+                    commandBuffer.EndSample(GaussianSplatRenderSystem.s_ProfCompose);
+                });
+            }
+        }
+
+        GSRenderPass m_Pass;
+        bool m_HasCamera;
+
+        public override void Create()
+        {
+            m_Pass = new GSRenderPass
+            {
+                renderPassEvent = RenderPassEvent.BeforeRenderingTransparents
+            };
+        }
+
+        public override void OnCameraPreCull(ScriptableRenderer renderer, in CameraData cameraData)
+        {
+            m_HasCamera = false;
+            var system = GaussianSplatRenderSystem.instance;
+            if (!system.GatherSplatsForCamera(cameraData.camera))
+                return;
+							   
+            m_HasCamera = true;
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            if (!m_HasCamera)
+                return;
+										 
+            renderer.EnqueuePass(m_Pass);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            m_Pass = null;
+        }
+    }
+    #else
     public class GaussianSplatURPFeature : ScriptableRendererFeature
     {
         public class GSRenderPass : ScriptableRenderPass
@@ -92,6 +185,7 @@ namespace GaussianSplatting.Runtime
             m_Pass = null;
         }
     }
+    #endif
 }
 
 #endif // #if GS_ENABLE_URP
